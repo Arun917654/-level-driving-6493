07248e6e2fe81f561d68f9d36e0ea52dc6d39791
arun yadav <ay917654@gmail.com>
2023-04-01 16:01:43 +0530

    fw25_284

--- product.js
+++ product.js
@@ -1,16 +1,15 @@
 let products=document.getElementById("products");
 let buttons=document.getElementById("buttons")
 let rangebtn=document.getElementById("range");
-
-
+let sortbyprice=document.querySelector(".select-by-price")
+let filterbycolor=document.getElementById("Color")
 // Fetch & render users
-
 async function  Fetchdata(url){
 try {
     let res=await  fetch(url);
+    console.log(res)
     let data=await res.json()
-    console.log(data)
-    apidata=data;
+   
    display(data)
 } catch (error) {
     console.log(error)
@@ -33,30 +32,58 @@ try {
     let data=await res.json()
     let sortedarr=[]
    for(let i=0;i<data.length;i++) {
-        if(+data[i].price<=value){
+        if(+data[i].Price<=value){
             sortedarr.push(data[i])
         }else if(value===""){
             Fetchdata("http://localhost:3000/posts")
         }
     }
     display(sortedarr)
-
-    
-//    display(data)
 } catch (error) {
     console.log(error)
 }
 
 }
 
-
-
-
+// ---------------------------sort by price------------------------
+sortbyprice.addEventListener("change",()=>{
+    if(sortbyprice.value==="asc"){
+        Fetchdata("http://localhost:3000/posts?_sort=Price&_order=desc")
+    }else if(sortbyprice.value==="desc"){
+        Fetchdata("http://localhost:3000/posts?_sort=Price&_order=asc")
+    }else{
+        Fetchdata("http://localhost:3000/posts")
+    }
+    
+})
+// -------------------------filter by color------------------------
+filterbycolor.addEventListener("change",()=>{
+    if(filterbycolor.value=="red"){
+        Fetchdata("http://localhost:3000/posts?color=red&author=typicode")
+    }
+    else if(filterbycolor.value=="blue"){
+        Fetchdata("http://localhost:3000/posts?color=blue&author=typicode")
+    }
+    else if(filterbycolor.value=="black"){
+        Fetchdata("http://localhost:3000/posts?color=black&author=typicode")
+    }
+    else if(filterbycolor.value=="green"){
+        Fetchdata("http://localhost:3000/posts?color=green&author=typicode")
+    }
+    else if(filterbycolor.value=="gold"){
+        Fetchdata("http://localhost:3000/posts?color=gold&author=typicode")
+    }
+    else if(filterbycolor.value=="silver"){
+        Fetchdata("http://localhost:3000/posts?color=silver&author=typicode")
+    }else{
+        Fetchdata("http://localhost:3000/posts")
+    }
+})
 // ----------------------  display function-----------------------------
 function display(data){
     products.innerHTML=null;
      data.forEach(e => {
-        products.append(makecards(e.image,e.name,e.price))
+        products.append(makecards(e.Image,e.name,e.Price))
         
     });
 }
